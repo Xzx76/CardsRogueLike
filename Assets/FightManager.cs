fe085318b8fc69dbe04543a999b9837cb5ae7e95
忠翔 徐 <2467512064@qq.com>
2023-04-13 17:45:09 +0800

    使用委托作回合结算方式

--- Assets/FightManager.cs
+++ Assets/FightManager.cs
@@ -19,6 +19,8 @@ public class FightManager : MonoBehaviour
     [SerializeField]
     public int roundHeadCardNum;
 
+    private Action roundOver;
+
     private int seed = 24675120;
     private System.Random random;
     private List<GameObject> headCards;//手卡物体
